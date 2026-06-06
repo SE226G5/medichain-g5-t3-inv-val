725d30d51d1220b1e321c9434d0bb4cb9526d306
lana ahmad <lana512004ahmad@gmail.com>
2026-06-06 11:47:02 +0300

    Refactor MaterialEvaluatorTests for clarity and accuracy

--- tests/MaterialEvaluatorTests.cs
+++ tests/MaterialEvaluatorTests.cs
@@ -1,32 +1,44 @@
 using System;
-using Xunit;         
-using MaterialApp;   
+using Xunit;
+using MaterialApp;
 
 namespace MaterialApp.Tests
 {
     public class MaterialEvaluatorTests
     {
+
         [Fact]
-        public void Test_Expired()
+        public void Test_Expired_Case()
         {
             var evaluator = new MaterialEvaluator();
-            var result = evaluator.ValidateMaterialExpiry(DateTime.Today.AddDays(-2), 5);
+            var result = evaluator.ValidateMaterialExpiry(DateTime.Today.AddDays(-2), 5, false);
             Assert.Equal("Expired", result);
         }
 
         [Fact]
-        public void Test_NearExpiry()
+        public void Test_NearExpiry_NormalMaterial()
         {
             var evaluator = new MaterialEvaluator();
-            var result = evaluator.ValidateMaterialExpiry(DateTime.Today.AddDays(2), 5);
+            var result = evaluator.ValidateMaterialExpiry(DateTime.Today.AddDays(2), 5, false);
             Assert.Equal("Near Expiry", result);
         }
 
+
         [Fact]
-        public void Test_Valid()
+        public void Test_NearExpiry_CriticalMaterial_WithSafetyDays()
         {
             var evaluator = new MaterialEvaluator();
-            var result = evaluator.ValidateMaterialExpiry(DateTime.Today.AddDays(10), 5);
+
+            var result = evaluator.ValidateMaterialExpiry(DateTime.Today.AddDays(7), 5, true);
+            Assert.Equal("Near Expiry", result);
+        }
+
+        
+        [Fact]
+        public void Test_Valid_Case()
+        {
+            var evaluator = new MaterialEvaluator();
+            var result = evaluator.ValidateMaterialExpiry(DateTime.Today.AddDays(15), 5, false);
             Assert.Equal("Valid", result);
         }
     }
