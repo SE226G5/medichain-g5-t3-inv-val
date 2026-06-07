726b60c1b2151b56fbd7196ef022e8129b09846b
Heaven Fandi <166942804+HeavenFandi@users.noreply.github.com>
2026-06-07 19:49:51 +0300

    Refactor source function

--- src/TestFeasibilityValidator.java
+++ src/TestFeasibilityValidator.java
@@ -1,23 +1,23 @@
 public class TestFeasibilityValidator {
 
-    public static String validateTestFeasibility(int availableQuantity, int requiredQuantity, boolean expired, boolean alternativeLabAvailable) {
+    public static String validateTestFeasibility(
+            int availableQuantity,
+            int requiredQuantity,
+            boolean expired,
+            boolean alternativeLabAvailable) {
 
-        if (availableQuantity <= 0) {
-            if (alternativeLabAvailable) {
-                return "Transfer to another lab";
-            } else {
-                return "Postpone test";
-            }
-        } else if (availableQuantity < requiredQuantity) {
-            if (alternativeLabAvailable) {
-                return "Transfer to another lab";
-            } else {
-                return "Postpone test";
-            }
-        } else if (expired) {
+        if (expired)
             return "Reject test - material expired";
-        } else {
-            return "Test approved";
-        }
+
+        boolean unavailable =
+                availableQuantity <= 0 ||
+                availableQuantity < requiredQuantity;
+
+        if (unavailable)
+            return alternativeLabAvailable
+                    ? "Transfer to another lab"
+                    : "Postpone test";
+
+        return "Test approved";
     }
 }
